308f8d70fb9db21e13ebe393a9ded4b2ebdd494a
Ashley Zink <pamperedzink@gmail.com>
2020-12-04 13:37:38 -0700

    Create Spring Boot REST

---
+++ DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/DoggieMeetupApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.doggiemeetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggieMeetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggieMeetupApplication.class, args);
+	}
+
+}
---
+++ DoggieMeetup/src/test/java/com/skilldistillery/doggiemeetup/DoggieMeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.doggiemeetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggieMeetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
