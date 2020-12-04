ee4d7981902032cd2efa2cb07faf76b70c07f66f
Jourdan Rentschler <jrentschler3@gmail.com>
2020-12-04 15:41:06 -0700

    Configure CrossOrigin.

--- DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/controllers/TestController.java
+++ DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/controllers/TestController.java
@@ -1,9 +1,11 @@
 package com.skilldistillery.doggiemeetup.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin({ "*", "http://localhost:4210" })
 @RequestMapping("api")
 @RestController
 public class TestController {
