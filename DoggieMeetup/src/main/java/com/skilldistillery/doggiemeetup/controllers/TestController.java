4060d94fff3ca90f5f7fe29004ba203fa81a908c
Ashley Zink <pamperedzink@gmail.com>
2020-12-04 13:49:45 -0700

    Create test endpoint

---
+++ DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.doggiemeetup.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api")
+@RestController
+public class TestController {
+
+	@GetMapping("ping")
+	public String ping() {
+		return "pong";
+	}
+	
+}
