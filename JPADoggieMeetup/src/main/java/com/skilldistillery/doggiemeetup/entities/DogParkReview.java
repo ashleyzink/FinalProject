789f76bf77979aaeee48ac14071a8838fdf55c6c
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-04 19:18:13 -0700

    DogParkReview entity, DogParkReviewId embeddable composite key class

---
+++ JPADoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/entities/DogParkReview.java
@@ -0,0 +1,114 @@
+package com.skilldistillery.doggiemeetup.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class DogParkReview {
+	
+	@EmbeddedId
+	private DogParkReviewId id;
+	
+	private Integer rating;
+	
+	private String review;
+	
+	@Column(name="img_url")
+	private String imgUrl;
+	
+	@Column(name="review_date")
+	@CreationTimestamp
+	private LocalDateTime reviewDate;
+
+	public DogParkReviewId getId() {
+		return id;
+	}
+
+	public void setId(DogParkReviewId id) {
+		this.id = id;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public LocalDateTime getReviewDate() {
+		return reviewDate;
+	}
+
+	public void setReviewDate(LocalDateTime reviewDate) {
+		this.reviewDate = reviewDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DogParkReview other = (DogParkReview) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("DogParkReview [id=");
+		builder.append(id);
+		builder.append(", rating=");
+		builder.append(rating);
+		builder.append(", review=");
+		builder.append(review);
+		builder.append(", imgUrl=");
+		builder.append(imgUrl);
+		builder.append(", reviewDate=");
+		builder.append(reviewDate);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public DogParkReview() {
+		super();
+	}
+
+}
