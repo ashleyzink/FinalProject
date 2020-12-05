58d4e532b5f9991d9fcdccf6b0efab34ed257caa
Ashley Zink <pamperedzink@gmail.com>
2020-12-04 18:19:10 -0700

    User, DogPark, Address, Route entities completed

---
+++ JPADoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/entities/DogPark.java
@@ -0,0 +1,119 @@
+package com.skilldistillery.doggiemeetup.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class DogPark {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	
+	@Column(name="off_leash")
+	private Boolean offLeash;
+	
+	@Column(name="img_url")
+	private String imageUrl;
+
+	//CONSTRUCTORS ------------------------------------------------------------------------------------
+
+	public DogPark() {
+		super();
+	}
+
+	//GETTERS/SETTERS ---------------------------------------------------------------------------------
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getOffLeash() {
+		return offLeash;
+	}
+
+	public void setOffLeash(Boolean offLeash) {
+		this.offLeash = offLeash;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	
+	//HASHCODE/EQUALS ---------------------------------------------------------------------------------
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DogPark other = (DogPark) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	//TOSTRING-------------------------------------------------------------------------------------------
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("DogPark [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", description=");
+		builder.append(description);
+		builder.append(", offLeash=");
+		builder.append(offLeash);
+		builder.append(", imageUrl=");
+		builder.append(imageUrl);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+}
--- JPADoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/entities/User.java
+++ JPADoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/entities/User.java
@@ -1,9 +1,14 @@
 package com.skilldistillery.doggiemeetup.entities;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 public class User {
@@ -12,6 +17,44 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String username;
+	private String email;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	private String password;
+	private String role;
+	private Boolean enabled;
+	
+	@Column(name="date_option")
+	private Boolean dateOption;
+	
+	@Column(name="profile_photo_url")
+	private String profilePhotoUrl;
+	private String bio;
+	
+	@CreationTimestamp
+	@Column(name="create_date")
+	private LocalDateTime createDate;
+	
+	@Column(name="private_profile")
+	private Boolean privateProfile;
+	
+	@Column(name="location_private")
+	private Boolean locationPrivate;
+	
+	//CONSTRUCTORS -------------------------------------------------------------------------------------
+	 
+	
+	public User() {
+		super();
+	}
+	
+	
+	//GETTERS/SETTERS-----------------------------------------------------------------------------------
 
 	public int getId() {
 		return id;
@@ -29,9 +72,151 @@ public class User {
 		this.username = username;
 	}
 
-	public User() {
-		super();
+
+	public String getEmail() {
+		return email;
 	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+
+
+	public Boolean getDateOption() {
+		return dateOption;
+	}
+
+
+
+	public void setDateOption(Boolean dateOption) {
+		this.dateOption = dateOption;
+	}
+
+
+
+	public String getProfilePhotoUrl() {
+		return profilePhotoUrl;
+	}
+
+
+
+	public void setProfilePhotoUrl(String profilePhotoUrl) {
+		this.profilePhotoUrl = profilePhotoUrl;
+	}
+
+
+
+	public String getBio() {
+		return bio;
+	}
+
+
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+
+
+	public Boolean getPrivateProfile() {
+		return privateProfile;
+	}
+
+
+
+	public void setPrivateProfile(Boolean privateProfile) {
+		this.privateProfile = privateProfile;
+	}
+
+
+
+	public Boolean getLocationPrivate() {
+		return locationPrivate;
+	}
+
+
+
+	public void setLocationPrivate(Boolean locationPrivate) {
+		this.locationPrivate = locationPrivate;
+	}
+
+
+	//HASHCODE/EQUALS (ID ONLY) ------------------------------------------------------------------------
 
 	@Override
 	public int hashCode() {
@@ -63,7 +248,37 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + "]";
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [id=");
+		builder.append(id);
+		builder.append(", username=");
+		builder.append(username);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append(", firstName=");
+		builder.append(firstName);
+		builder.append(", lastName=");
+		builder.append(lastName);
+		builder.append(", password=");
+		builder.append(password);
+		builder.append(", role=");
+		builder.append(role);
+		builder.append(", enabled=");
+		builder.append(enabled);
+		builder.append(", dateOption=");
+		builder.append(dateOption);
+		builder.append(", profilePhotoUrl=");
+		builder.append(profilePhotoUrl);
+		builder.append(", bio=");
+		builder.append(bio);
+		builder.append(", createDate=");
+		builder.append(createDate);
+		builder.append(", privateProfile=");
+		builder.append(privateProfile);
+		builder.append(", locationPrivate=");
+		builder.append(locationPrivate);
+		builder.append("]");
+		return builder.toString();
 	}
 
 }
