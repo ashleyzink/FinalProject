6ebef9a07afc8fef7a778fb9e5886ac4fa99b6e1
Ashley Zink <pamperedzink@gmail.com>
2020-12-04 19:51:38 -0700

    Added address_id mapping to DogPark/Test, fixed bugs

--- JPADoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/entities/DogPark.java
+++ JPADoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/entities/DogPark.java
@@ -5,8 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="dog_park")
 public class DogPark {
 
 	@Id
@@ -18,8 +22,12 @@ public class DogPark {
 	@Column(name="off_leash")
 	private Boolean offLeash;
 	
-	@Column(name="img_url")
+	@Column(name="image_url")
 	private String imageUrl;
+	
+	@OneToOne
+	@JoinColumn(name="address_id")
+	private Address address;
 
 	//CONSTRUCTORS ------------------------------------------------------------------------------------
 
@@ -68,11 +76,19 @@ public class DogPark {
 	public void setImageUrl(String imageUrl) {
 		this.imageUrl = imageUrl;
 	}
+	
+	
+	public Address getAddress() {
+		return address;
+	}
 
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
 	
 	//HASHCODE/EQUALS ---------------------------------------------------------------------------------
-	
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
