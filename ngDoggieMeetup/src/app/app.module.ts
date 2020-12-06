46f35cc17cdcc39b177dcdbe1ee62df2c55b9895
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-06 13:14:19 -0700

    navigation bar stub added

--- ngDoggieMeetup/src/app/app.module.ts
+++ ngDoggieMeetup/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
