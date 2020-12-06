ffaf8ee57fd2de16831c48a2e049733d9a6e361d
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-06 12:14:07 -0700

    added @ng-bootstrap/ng-bootstrap

--- ngDoggieMeetup/src/app/app.module.ts
+++ ngDoggieMeetup/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
