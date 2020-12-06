3aa9ba4717089fc933c6c4546e2a3ebfc623b403
Jourdan Rentschler <jrentschler3@gmail.com>
2020-12-06 11:43:44 -0700

    Completed DogParkReviewService/Impl/Repo.

--- DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/repositories/DogParkCommentRepository.java
+++ DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/repositories/DogParkCommentRepository.java
@@ -1,9 +1,14 @@
 package com.skilldistillery.doggiemeetup.repositories;
 
+
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.doggiemeetup.entities.DogParkComment;
 
 public interface DogParkCommentRepository extends JpaRepository<DogParkComment, Integer> {
-
+	List<DogParkComment> findbyUser_Username(String username);
+	DogParkComment findByUser_UsernameAndId(String username, int dogParkCommentId);
 }
