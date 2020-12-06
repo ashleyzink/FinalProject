b5bd60fb7bb8bb2e388ac2225e1a65f7c5bee470
Jourdan Rentschler <jrentschler3@gmail.com>
2020-12-05 17:23:02 -0700

    DogParkReviewService/Impl completed.

--- DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/services/DogParkReviewServiceImpl.java
+++ DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/services/DogParkReviewServiceImpl.java
@@ -1,5 +1,50 @@
 package com.skilldistillery.doggiemeetup.services;
 
-public class DogParkReviewServiceImpl implements DogParkReviewService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.doggiemeetup.entities.DogParkReview;
+import com.skilldistillery.doggiemeetup.repositories.DogParkReviewRepository;
+import com.skilldistillery.doggiemeetup.repositories.UserRepository;
+@Service 
+public class DogParkReviewServiceImpl implements DogParkReviewService {
+	
+	@Autowired
+	private DogParkReviewRepository dogParkReviewRepo;
+	
+	@Autowired 
+	private UserRepository userRepo;
+
+	@Override
+	public List<DogParkReview> index() {
+		return null;
+	}
+
+	@Override
+	public DogParkReview show(int dogParkId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DogParkReview create(String username, DogParkReview dogParkReview) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DogParkReview update(String username, int dogParkId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean destroy(String username, int dogParkId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
 }
