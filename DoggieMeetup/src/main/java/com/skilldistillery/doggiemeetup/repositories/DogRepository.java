525023a5e42807245801fdfa2d97079cabf3b40d
Ashley Zink <pamperedzink@gmail.com>
2020-12-05 22:09:08 -0700

    Updated Dog Service/Impl with CRUD and Search for all dogs, Get dogs by user

--- DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/repositories/DogRepository.java
+++ DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/repositories/DogRepository.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.doggiemeetup.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.doggiemeetup.entities.Dog;
 
 public interface DogRepository extends JpaRepository<Dog, Integer> {
+	
+	List<Dog> findByUser_Username(String username);
+	
+	Dog findByUser_UsernameAndId(String username, int dogId);
 
 }
--- DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/services/DogServiceImpl.java
+++ DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/services/DogServiceImpl.java
@@ -1,5 +1,83 @@
 package com.skilldistillery.doggiemeetup.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.doggiemeetup.entities.Dog;
+import com.skilldistillery.doggiemeetup.entities.User;
+import com.skilldistillery.doggiemeetup.repositories.DogRepository;
+import com.skilldistillery.doggiemeetup.repositories.UserRepository;
+
+@Service
 public class DogServiceImpl implements DogService {
+	
+	@Autowired
+	private DogRepository dogRepo;
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+
+	@Override
+	public List<Dog> index(String username) {
+		if (userRepo.findByUsername(username) == null) {
+			return null;
+		}
+		return dogRepo.findByUser_Username(username);
+	}
+
+	@Override
+	public List<Dog> getAllDogs() {
+		return dogRepo.findAll();
+	}
+
+	@Override
+	public Dog show(String username, int dogId) {
+		return dogRepo.findByUser_UsernameAndId(username, dogId);
+	}
+
+	@Override
+	public Dog create(String username, Dog dog) {
+		User user = userRepo.findByUsername(username);
+		if (user != null) {
+			dog.setUser(user);
+			dogRepo.saveAndFlush(dog);
+		}
+		return dog;
+	}
+
+	@Override
+	public Dog update(String username, int dogId, Dog dog) {
+		Dog managedDog = dogRepo.findByUser_UsernameAndId(username, dogId);
+		if (managedDog != null) {
+			managedDog.setName(dog.getName());
+			managedDog.setBreed(dog.getBreed());
+			managedDog.setTemperament(dog.getTemperament());
+			managedDog.setProfilePicUrl(dog.getProfilePicUrl());
+			managedDog.setActivityLevel(dog.getActivityLevel());
+			managedDog.setSize(dog.getSize());
+			managedDog.setBio(dog.getBio());
+			managedDog.setBirthday(dog.getBirthday());
+			managedDog.setRainbowBridge(dog.getRainbowBridge());
+			managedDog.setCreateDate(dog.getCreateDate());
+			dogRepo.saveAndFlush(managedDog);
+		}
+		return managedDog;
+	}
+
+	@Override
+	public boolean destroy(String username, int dogId) {
+		boolean deleted = false;
+		Dog dog = dogRepo.findByUser_UsernameAndId(username, dogId);
+		if (dog != null) {
+			dogRepo.delete(dog);
+			deleted = true;
+		}
+		return false;
+	}
+	
+
 
 }
