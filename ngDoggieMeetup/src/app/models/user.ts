27c6531fa80a4f5df7ada5b4e6713c675e5af6e5
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-06 16:33:41 -0700

    partial user entity model

--- ngDoggieMeetup/src/app/models/user.ts
+++ ngDoggieMeetup/src/app/models/user.ts
@@ -1,2 +1,41 @@
 export class User {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  enabled: boolean;
+  role: string;
+  firstName: string;
+  lastName: string;
+  dateOption: boolean;
+  profilePhotoUrl: string;
+  bio: string;
+  // TODO address, createDate, profilePrivate, locationPrivate
+  // TODO relationships?
+
+  constructor(
+    id?: number,
+    username?: string,
+    password?: string,
+    email?: string,
+    enabled?: boolean,
+    role?: string,
+    firstName?: string,
+    lastName?: string,
+    dateOption?: boolean,
+    profilePhotoUrl?: string,
+    bio?: string,
+  ) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.enabled = enabled;
+    this.role = role;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOption = dateOption;
+    this.profilePhotoUrl = profilePhotoUrl;
+    this.bio = bio;
+  }
 }
