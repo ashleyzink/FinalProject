7058298faafdef3ad4428dfab42612deb606e053
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-08 01:12:38 -0700

    dog park service crud, html formatting

--- ngDoggieMeetup/src/app/services/dog-park.service.ts
+++ ngDoggieMeetup/src/app/services/dog-park.service.ts
@@ -39,16 +39,66 @@ export class DogParkService {
     }
 
 
-    public index(): Observable<DogPark[]> {
-      if(!this.authService.checkLogin) {
-        this.router.navigateByUrl('/login');
-      }
+    index(): Observable<DogPark[]> {
       const httpOptions = this.getHttpOptions();
       return this.http.get<DogPark[]>(this.url, httpOptions).pipe(
         catchError((err: any) => {
           console.log(err);
-          return throwError('error loading dog parks');
+          return throwError('error getting dog parks index');
         })
       );
     }
+
+    show(id: number): Observable<DogPark> {
+      const httpOptions = this.getHttpOptions();
+      return this.http.get<DogPark>(this.url + '/' + id, httpOptions).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('error getting dog park id: ' + id);
+        })
+      );
+    }
+
+    create(newDogPark: DogPark): Observable<DogPark> {
+      if(!this.authService.checkLogin) {
+        console.log('not logged in at DogParkService.create() : redirect to /login')
+        this.router.navigateByUrl('/login');
+      }
+      const httpOptions = this.getAuthHttpOptions();
+      return this.http.post<DogPark>(this.url, newDogPark, httpOptions).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('error creating dog park');
+        })
+      );
+    }
+
+    update(dogPark: DogPark): Observable<DogPark> {
+      if(!this.authService.checkLogin) {
+        console.log('not logged in at DogParkService.update() : redirect to /login')
+        this.router.navigateByUrl('/login');
+      }
+      const httpOptions = this.getAuthHttpOptions();
+      return this.http.put<DogPark>(this.url + '/' + dogPark.id, dogPark, httpOptions).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('error updating dog park id: ' + dogPark.id);
+        })
+      );
+    }
+
+
+  public delete(id: number): Observable<Object> {
+    if(!this.authService.checkLogin) {
+      console.log('not logged in at DogParkService.delete() : redirect to /login')
+      this.router.navigateByUrl('/login');
+    }
+    const httpOptions = this.getAuthHttpOptions();
+    return this.http.delete(this.url + '/' + id, httpOptions).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('Error deleting dog park id: ' + id);
+      })
+    );
+  }
 }
