0d36b7dd77723e08527d6fbd909ce337fb11a371
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-08 15:13:54 -0700

    general comment model

--- ngDoggieMeetup/src/app/models/general-comment.ts
+++ ngDoggieMeetup/src/app/models/general-comment.ts
@@ -1,2 +1,25 @@
+import { User } from './user';
+
 export class GeneralComment {
+  id: number;
+  commentDate: string;
+  commentText: string;
+  title: string;
+  user: User;
+  replyToComment: GeneralComment;
+  constructor(
+    id?: number,
+    commentDate?: string,
+    commentText?: string,
+    title?: string,
+    user?: User,
+    replyToComment?: GeneralComment,
+  ) {
+    this.id = id;
+    this.commentDate = commentDate;
+    this.commentText = commentText;
+    this.title = title;
+    this.user = user;
+    this.replyToComment = replyToComment;
+  }
 }
