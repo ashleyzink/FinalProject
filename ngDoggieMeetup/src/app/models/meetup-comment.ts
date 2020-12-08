857b3d6430055212ea35653fb6ef3165dea6bc68
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-08 15:12:24 -0700

    meetup comment model

--- ngDoggieMeetup/src/app/models/meetup-comment.ts
+++ ngDoggieMeetup/src/app/models/meetup-comment.ts
@@ -1,2 +1,29 @@
+import { Meetup } from './meetup';
+import { User } from './user';
+
 export class MeetupComment {
+  id: number;
+  commentDate: string;
+  commentText: string;
+  title: string;
+  user: User;
+  replyToComment: MeetupComment;
+  meetup: Meetup;
+  constructor(
+    id?: number,
+    commentDate?: string,
+    commentText?: string,
+    title?: string,
+    user?: User,
+    replyToComment?: MeetupComment,
+    meetup?: Meetup,
+  ) {
+    this.id = id;
+    this.commentDate = commentDate;
+    this.commentText = commentText;
+    this.title = title;
+    this.user = user;
+    this.replyToComment = replyToComment;
+    this.meetup = meetup;
+  }
 }
