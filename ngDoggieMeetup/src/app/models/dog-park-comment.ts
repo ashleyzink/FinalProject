e77a5d9bd01a0cb2ed363479b0ea36382e565b26
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-08 15:09:36 -0700

    dog park comment model

--- ngDoggieMeetup/src/app/models/dog-park-comment.ts
+++ ngDoggieMeetup/src/app/models/dog-park-comment.ts
@@ -1,2 +1,28 @@
+import { DogPark } from './dog-park';
+import { User } from 'src/app/models/user';
 export class DogParkComment {
+  id: number;
+  commentDate: string;
+  commentText: string;
+  title: string;
+  user: User;
+  replyToComment: DogParkComment;
+  dogPark: DogPark;
+  constructor(
+    id?: number,
+    commentDate?: string,
+    commentText?: string,
+    title?: string,
+    user?: User,
+    replyToComment?: DogParkComment,
+    dogPark?: DogPark,
+  ) {
+    this.id = id;
+    this.commentDate = commentDate;
+    this.commentText = commentText;
+    this.title = title;
+    this.user = user;
+    this.replyToComment = replyToComment;
+    this.dogPark = dogPark;
+  }
 }
