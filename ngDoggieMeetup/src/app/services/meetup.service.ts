a221d6679a5fd1a8f419b45932ec762a4a71bfcb
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-10 13:50:09 -0700

    work on meetups

--- ngDoggieMeetup/src/app/services/meetup.service.ts
+++ ngDoggieMeetup/src/app/services/meetup.service.ts
@@ -1,6 +1,11 @@
+import { DogPark } from './../models/dog-park';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Meetup } from '../models/meetup';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -8,7 +13,32 @@ import { AuthService } from './auth.service';
 })
 export class MeetupService {
 
-  constructor(private http: HttpClient,
-    private authService: AuthService,
-    private router: Router) { }
+  private url = environment.baseUrl + 'api/meetups/'
+  private authUrl = environment.baseUrl + 'api/auth/meetups/'
+
+  constructor(private http: HttpClient, private authService: AuthService, private router: Router) {
+
+  }
+
+  index(dogPark?: DogPark): Observable<Meetup[]> {
+    const httpOptions = this.authService.getHttpOptions();
+    if (!dogPark) {
+      return this.http.get<Meetup[]>(this.url, httpOptions).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('error getting meetups');
+        })
+      );
+    } else {
+      return this.http.get<Meetup[]>(this.url, httpOptions).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('error getting meetups');
+        })
+      );
+    }
+  }
+
+
+
 }
