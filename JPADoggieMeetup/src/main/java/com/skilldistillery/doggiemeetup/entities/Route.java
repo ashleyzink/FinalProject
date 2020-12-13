fbe64d80f1beed8751814986162fb8a16fd6956a
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-12 23:45:48 -0700

    route and location entities updated, route controller added, tracking working?

--- JPADoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/entities/Route.java
+++ JPADoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/entities/Route.java
@@ -12,7 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 public class Route {
@@ -31,7 +31,7 @@ public class Route {
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	@JsonIgnore
+	@JsonIgnoreProperties({"route"})
 	@OneToMany(mappedBy = "route")
 	private List<Location> locations;
 
