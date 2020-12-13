fa4592e1c546f43b586dc82124a3bab032be3e00
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-12 21:34:47 -0700

    added location and route models

--- ngDoggieMeetup/src/app/models/location.ts
+++ ngDoggieMeetup/src/app/models/location.ts
@@ -1,2 +1,17 @@
 export class Location {
+  id: number;
+  lat: number;
+  lng: number;
+  pointTime: string;
+  constructor(
+    id?: number,
+    lat?: number,
+    lng?: number,
+    pointTime?: string,
+  ){
+    this.id = id;
+    this.lat = lat;
+    this.lng = lng;
+    this.pointTime = pointTime;
+  }
 }
