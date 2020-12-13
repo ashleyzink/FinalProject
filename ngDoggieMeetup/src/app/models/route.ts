9e9be0b5bde1e0330fbfb8c18df368ff44b1d84f
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-12 21:34:47 -0700

    added location and route models

--- ngDoggieMeetup/src/app/models/route.ts
+++ ngDoggieMeetup/src/app/models/route.ts
@@ -1,2 +1,23 @@
+import { strict } from 'assert';
+import { User } from './user';
+
 export class Route {
+  id: number;
+  startTime: string;
+  endTime: string;
+  user: User;
+  locations: Location[];
+  constructor(
+    id?: number,
+    startTime?: string,
+    endTime?: string,
+    user?: User,
+    locations?: Location[],
+  ) {
+    this.id = id;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.user = user;
+    this.locations = locations;
+  }
 }
