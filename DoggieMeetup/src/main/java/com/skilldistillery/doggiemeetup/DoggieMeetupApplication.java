dc90c42c08fb721759459352ad1da17aac289990
Daniel Majewski <d.majewski.3453@gmail.com>
2020-12-13 11:25:13 -0700

    ec2 config

--- DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/DoggieMeetupApplication.java
+++ DoggieMeetup/src/main/java/com/skilldistillery/doggiemeetup/DoggieMeetupApplication.java
@@ -2,12 +2,19 @@ package com.skilldistillery.doggiemeetup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DoggieMeetupApplication {
+public class DoggieMeetupApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DoggieMeetupApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoggieMeetupApplication.class, args);
